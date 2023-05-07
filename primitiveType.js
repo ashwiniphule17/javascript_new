fd22fed20eb406588023a8df1ae0e7bcaed42f93
ashwiniphule17 <ashwiniphule17@gmail.com>
2023-05-07 16:37:33 +0530

    created primitive type file
    created primitive type file

---
+++ primitiveType.js
@@ -0,0 +1,54 @@
+
+
+
+//1.Primitive Type
+// 1. Number
+var age = 31;
+var weight = 78;
+var height = 6.2;
+var specnumber = 0.50;
+console.log(age,weight,height,specnumber);
+
+var typeaOfAge = typeof age;
+console.log(typeaOfAge);
+ 
+console.log(typeof specnumber);
+ 
+console.log("Age",age);
+console.log("weight",weight);
+console.log("Height",height);
+console.log("SpecNumber",specnumber);
+
+//2. Boolean --> true or false
+var isMarried = true;
+var knowAngulae = true;
+var isSoftEngineer = true;
+var isBachelor = false;
+console.log(isBachelor);
+
+var typeOfIsMarried = typeof isMarried;
+console.log("Type of isMarried",typeOfIsMarried);
+ 
+var typeOfIsBachelor = typeof isBachelor;
+console.log("type of isBachelor",typeof isBachelor);
+
+//3.String -->sequence of character inclosed in "" OR ''
+var firstName = "Bill";
+var position = 'Microsoft Co-founder';
+var typeOfFirstName = typeof firstName;
+var city = '';
+console.log("Type of City", typeof city);
+
+console.log("Type of Variable--> firstName",typeof firstName);
+console.log("Type of Variable--> position",typeof position);
+
+//2. Special Data Type
+//1.Undefined
+var address;
+console.log("Address",address);
+console.log("Type of variable-->Address is:",typeof address);
+
+//2.Null
+var pin = null;
+console.log("Pin",pin);
+console.log("Type of variable pin is:",typeof pin);
