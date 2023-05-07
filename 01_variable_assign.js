df2b1f0596a61a6597deb894523318dfeb34ea25
ashwiniphule17 <ashwiniphule17@gmail.com>
2023-05-07 15:42:24 +0530

    updated basic info
    updated basic info

---
+++ 01_variable_assign.js
@@ -0,0 +1,40 @@
+
+
+
+
+var myFullName;
+myFullName = "Ashwini Jadhav"
+console.log(myFullName);
+
+var myFullName;
+myFullName = "Ashwini Dinesh Jadhav" //update variable
+console.log(myFullName);
+
+var myLove;
+myLove = "I Love Only JS";
+console.log(myLove);
+
+var firstName = "Ashwini";
+console.log(firstName);
+
+var lastName = "Jadhav";
+console.log(lastName);
+
+var age = "32";
+console.log(age);
+
+var address = "New Sangavi";
+console.log(address);
+
+var pincode = "411027";
+console.log(pincode);
+
+var gender = "Female";
+console.log(gender);
+
+var isMarried = "Yes";
+console.log(isMarried);
+
+
+
+
