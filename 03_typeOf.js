88bc5649201e2f04b309d9eca89a7994c85dfc8a
ashwiniphule17 <ashwiniphule17@gmail.com>
2023-05-07 19:17:32 +0530

    create type of js file
    create type of js file

---
+++ 03_typeOf.js
@@ -0,0 +1,38 @@
+
+
+
+
+var bankName;
+console.log(bankName);
+var typeOfBankName = typeof bankName;
+console.log('Type of Bank Name = ',typeOfBankName);
+
+var bankName = 'SBI bank';
+console.log(bankName);
+var typeOfBankName = typeof bankName;
+console.log('Type of Bank Name = ',typeOfBankName);
+
+var marks = "90%";
+console.log(marks);
+var typeOfMarks = typeof marks;
+console.log("Type Of Variable Marks = ",typeOfMarks);
+
+var isWorking;
+console.log(isWorking);
+var typeOfIsWorking = typeof isWorking;
+console.log("Type Of Is Working =",typeof isWorking);
+
+var isWorking = true;
+console.log(isWorking);
+
+var toatlCount = "hundred And Seven";
+console.log(toatlCount);
+var typeOfToatlCount = typeof toatlCount;
+console.log('Type of total Count =',typeof toatlCount);
+
+var toatlCount = 107;
+console.log(toatlCount);
+var typeOfToatlCount = typeof toatlCount;
+console.log('Type of total Count =',typeof toatlCount);
+
+
