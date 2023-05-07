104dcbae8d5ab8e5fd2c88d02448b90568180413
ashwiniphule17 <ashwiniphule17@gmail.com>
2023-05-07 15:16:06 +0530

    created hello.js
    created hello.js

---
+++ hello.js
@@ -0,0 +1,20 @@
+
+
+
+
+console.log("Ashwini Jadhav..");
+
+var firstName = "Ashwini";
+console.log(firstName);
+
+var age; // Variable Declaration 
+age = "29";  // Variable Initialization
+console.log(age);
+
+var holder; // Variable Declaration
+console.log(holder); // undefined
+holder = "My Spec"; // Variable Initialization
+console.log(holder);
+
+holder = "Ashwini Spec"; // Variable update
+console.log(holder); //
