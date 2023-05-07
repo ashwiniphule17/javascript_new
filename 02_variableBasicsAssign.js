25bec75f0daa549a199c786c4b19e6d60aa320fc
ashwiniphule17 <ashwiniphule17@gmail.com>
2023-05-07 18:05:33 +0530

    declare type number & string
    declare type number & string

---
+++ 02_variableBasicsAssign.js
@@ -0,0 +1,56 @@
+
+
+
+
+//*Number*
+console.log("///---- Number ----///");
+var pin = 413101;
+var srNo = 85;
+var mobileNumber = 9850527913;
+
+console.log(pin);
+var typeOfPin = typeof pin;
+console.log("Type =",typeof pin);
+
+console.log(srNo);
+var typeOfSrNo = typeof srNo;
+console.log("Type =",typeof srNo);
+
+console.log(mobileNumber);
+var typeOfMobilenumber= typeof mobileNumber;
+console.log("Type =",typeof mobileNumber);
+
+console.log("///---- Boolean ----///");
+var isSwimmer = false;
+var isSinger = false;
+var isDancer = true;
+
+console.log(isSwimmer);
+var typeOfIsSwimmer = typeof isSwimmer;
+console.log("Type of Swimmer=",typeof isSwimmer);
+
+console.log(isSinger);
+var typeOfIsSinger = typeof isSinger;
+console.log("Type of Singer=",typeof isSinger);
+
+console.log(isDancer);
+var typeOfIsDancer = typeof isDancer;
+console.log("Type of Dancer=",typeof isDancer);
+
+console.log('////---- String ----///');
+var graduation = "Engineering";
+var nationality = "Indian";
+var motherTounge = "Marathi";
+
+console.log("Type =",typeof graduation);
+console.log(graduation);
+
+console.log("Type =",typeof nationality);
+console.log(nationality);
+
+console.log("Type =",typeof motherTounge);
+console.log(motherTounge);
+
+console.log('Diff Between Undefined Vs Null');
+console.log("Undefined = When we declare the variable and dosen't initialize its value i.e.Undefined")
+console.log('Null = null value means there is empty or no value') ;
