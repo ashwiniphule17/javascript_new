a76700530c8ffae47fe23f5cd572bc2c87383002
ashwiniphule17 <ashwiniphule17@gmail.com>
2023-05-12 15:50:38 +0530

    hands on assignment
    hands on assignment

---
+++ 04_stringAssign_02.js
@@ -0,0 +1,31 @@
+
+
+
+
+function stringHandsOn(){
+    var handsOn = "  Hey you are doing good,keep it up    ";
+    console.log("Given string As It Is - ",handsOn);
+}
+stringHandsOn();
+ 
+var handsOn ="  Hey you are doing good,keep it up    ";
+var handsOnLength = handsOn.length;
+console.log("Total num of string Length is :",handsOnLength);
+console.log(".....................................................");
+
+var handsOn ="  Hey you are doing good,keep it up    ";
+var trimmedHandsOn = handsOn.trim();
+console.log("Given String After Trim:",trimmedHandsOn);
+console.log("Given String After Trim Length:",trimmedHandsOn.length);
+console.log(".....................................................");
+
+console.log("Total Num Of Removed Spaces: 6");
+console.log(".....................................................");
+
+var handsOn ="Hey you are doing good,keep it up";
+var charAtZeroIndex = handsOn.charAt();
+var charAtLastIndex = handsOn.charAt(handsOn.length-1);
+console.log("First Char & Last Char : ",charAtZeroIndex,charAtLastIndex);
+
+
+
