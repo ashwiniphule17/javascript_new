737ff613a5ab6d62fc895b13a13d916a5d822713
ashwiniphule17 <ashwiniphule17@gmail.com>
2023-05-12 13:29:49 +0530

    using string display hobbies
    using string display hobbies

---
+++ 04_stringBasicsAssign_01.js
@@ -0,0 +1,21 @@
+
+
+
+
+function myDreamCompany (){
+    var companyname = "UBS";
+    console.log("My Dream Company Is :",companyname);
+}
+myDreamCompany();
+console.log("                               ");
+
+function myHobbies(){
+    var myHobbies ="Travelling Cooking Listening Music";
+    console.log("My Hobbies Are :" , myHobbies);
+}
+myHobbies();
+console.log("                               ");
+
+var myHobbies = "Travelling Cooking Listening Music";
+var myHobbiesLength = myHobbies.length;
+console.log("Sum Of Total Num Of Character -",myHobbiesLength);
