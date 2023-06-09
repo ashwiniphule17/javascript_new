20f5cdf8d6ef0d8bad31a59da2ec98b954734261
Ashwini Jadhav <ashwiniphule17@gmail.com>
2023-06-09 19:22:37 +0530

    call fun sorting method
    call fun sorting method

---
+++ 03_callFunMethod.js
@@ -0,0 +1,22 @@
+
+
+
+
+const person = {
+    name: "Stew",
+    age: 54
+}
+function sayHello(company,role) {
+    console.log(` ${person.name} ${person.age}  ${company} ${role}`);
+}
+
+console.log("---------------------call()------------");
+sayHello.call(person, "Apple" )
+
+console.log("---------------------apply()------------");
+sayHello.apply(person, ["Apple" , "CEO"] );
+
+console.log("-----bind()---------");
+let funRef = sayHello.bind(person);
+
+funRef("Apple" , "CEO" );
