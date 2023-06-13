8683ee61fb56a78609febdb314468215ff0f9dfd
Ashwini Jadhav <ashwiniphule17@gmail.com>
2023-06-13 15:22:33 +0530

    dom assignment
    dom assignment

--- 15_arrayClone.js
+++ 15_arrayClone.js
@@ -14,3 +14,4 @@ const clonedArray = [...array];
 clonedArray.splice(2, 0, 100, 200);
 console.log(array);
 console.log(clonedArray);
+
--- DOM/script.js
+++ DOM/script.js
@@ -1,10 +1,6 @@
 
 
 
-
-
-
-
 const elementPersonal = document.getElementById("personal");
 console.log(elementPersonal.innerHTML);
 
@@ -13,6 +9,7 @@ console.log(elementNameByClass[0].innerHTML);
 
 const elementH3 = document.querySelectorAll("p")
 console.log(elementH3[0].innerHTML);
+
 
 // querySelectorAll
 // querySelector
@@ -35,8 +32,42 @@ for (const element of elementsPara) {
 
 console.log(`======== document.querySelectorAll(".ceo")`);
 const elementsCeo = document.querySelectorAll(".ceo")
+
 elementsCeo.forEach( (element)=>{
     console.log(element.innerHTML);
 } );
 
+console.log(` Updating an element city`);
+const elementCity = document.querySelector("#city");
+elementCity.innerHTML = "CITY: Mumbai";
 
+console.log(` Changing an element attribute`);
+const elementLink = document.querySelector('.link');
+elementLink.setAttribute('href', 'https://www.linkedin.com/login');
+
+
+console.log('Changing CSS properties');
+const elementAddress = document.querySelector('#address');
+elementAddress.style.color = 'red';
+elementAddress.style.fontFamily = 'sans-serif';
+
+console.log(`Removing an element`);
+const elementHobbies = document.querySelector('.hobbies');
+const elementCricket = document.querySelector('#cricket');
+elementHobbies.removeChild(elementCricket);
+
+console.log(`Creating node`);
+const elementTimePass = document.createElement('p');
+const textNodeTimePass = document.createTextNode("TimePass is also my hobby, Please don't laugh");
+elementTimePass.appendChild(textNodeTimePass);
+
+elementHobbies.appendChild(elementTimePass);
+
+const elementContact = document.querySelector('#contact');
+elementContact.addEventListener('click', ()=>{
+    elementContact.style.color = 'green';
+});
+
+function show(){
+    alert('show function called');
+}
