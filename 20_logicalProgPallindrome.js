e3befb28e6caad3ba75794bea4057b28b3894684
Ashwini Jadhav <ashwiniphule17@gmail.com>
2023-06-13 15:23:07 +0530

    pallindrome assignment
    pallindrome assignment

---
+++ 20_logicalProgPallindrome.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+ function reverseStr(str) {
+    var reverseString = "";
+    var strLength = str.length-1;
+    for (let index = strLength; index >= 0; index--) {
+        var char = str.charAt(index);
+        reverseString = reverseString.concat(char);
+    }
+    if (reverseString==str) {
+        console.log(`Given string : ${str} is --Pallindrome`);
+    }
+    else if (reverseString!==str) {
+        console.log(`Given string : ${str} is -- Not Pallindrome`);
+    }
+ };
+ reverseStr("madam");
+ reverseStr("141");
+ reverseStr("Sunday");
+ reverseStr("mom");
+ reverseStr("listen");
+ reverseStr("dad");
